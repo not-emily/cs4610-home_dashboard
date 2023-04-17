65af1d3fa96e19c389affbae5d9c5827f921be3c
“Emily <emilybthomas11@aggiemail.usu.edu>
2023-04-17 09:59:28 -0600

    Add flipping state to cards

--- src/widgets/GroceryList.tsx
+++ src/widgets/GroceryList.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react"
 
 export const GroceryList = () => {
+    const [flip, setFlip] = useState(0)
+
     return (
         <>
             <div className="widget__header">
                 <div className="widget__title">Grocery List</div>
-                <button className="widget__action">+</button>
+                <div className="widget__action">
+                    {
+                        flip === 0 ? 
+                            <button onClick={() => setFlip(1)}>+</button>:
+                        flip === 1 ?
+                            <button onClick={() => setFlip(0)}>Back</button>:
+                            <></>
+                    }
+                </div>
             </div>
+
+                {
+                    flip === 0 ?
+                        <p>This is the front of the widget</p>:
+                    flip === 1 ?
+                        <p>This is the back of the widget</p>:
+                        <></>
+                }
         </>
     )
 }
