bb90fb633090f9d5d219e1b1a1e880a4a18a84d3
“Emily <emilybthomas11@aggiemail.usu.edu>
2023-04-17 10:56:12 -0600

    Connected database

--- src/widgets/GroceryList.tsx
+++ src/widgets/GroceryList.tsx
@@ -1,7 +1,36 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import UserContext from "../context/user";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../lib/firebase";
+
+
+type GroceryItem = {
+  id: string,
+  creatorId: string,
+  content: string,
+}
 
 export const GroceryList = () => {
+    const user = useContext(UserContext);
     const [flip, setFlip] = useState(0)
+    const [items, setItems] = useState<GroceryItem[]>([])
+    const [content, setContent] = useState("")
+    const [newContentName, setNewContentName] = useState("")
+
+
+    async function createGroceryItem() {
+        if (!content) {
+        return;
+        }
+        const item = {
+        content: content,
+        creatorId: user!.uid,
+        }
+        const docRef = await addDoc(collection(db, "grocery_items"), item);
+
+        (item as GroceryItem).id = docRef.id;
+        setItems([...items, item as GroceryItem]);
+    }
 
     return (
         <>
@@ -17,14 +46,22 @@ export const GroceryList = () => {
                     }
                 </div>
             </div>
-
-                {
-                    flip === 0 ?
-                        <p>This is the front of the widget</p>:
-                    flip === 1 ?
-                        <p>This is the back of the widget</p>:
-                        <></>
-                }
+            {
+                flip === 0 ?
+                    <p>This is the front of the widget</p>:
+                flip === 1 ?
+                    <form onSubmit={(e) => {
+                        if (content != null) {
+                        createGroceryItem()
+                        }
+                        setContent("")      // Reset text field after submission
+                        e.preventDefault()
+                    }}>
+                        <input type="text" placeholder="Grocery item..." value={content} onChange={(e) => setContent(e.target.value)} />
+                        <input type="submit" value="Save" />
+                    </form>:
+                    <></>
+            }
         </>
     )
 }
