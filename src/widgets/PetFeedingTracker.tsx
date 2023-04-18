92c101b0ad70835a3426396a4f54cdb6623dc99e
“Emily <emilybthomas11@aggiemail.usu.edu>
2023-04-18 15:43:13 -0600

    Stub out pet feeding tracker

---
+++ src/widgets/PetFeedingTracker.tsx
@@ -0,0 +1,179 @@
+import { useContext, useEffect, useState } from "react"
+import UserContext from "../context/user";
+import { addDoc, collection, deleteDoc, doc, getDocs, query, updateDoc, where } from "firebase/firestore";
+import { db } from "../lib/firebase";
+import { MdAdd, MdArrowBack } from 'react-icons/md'
+
+
+
+type Pet = {
+  id: string,
+  creatorId: string,
+  name: string,
+  freq_hr: number,
+}
+
+type Toast = {
+    message: string,
+    type: "success" | "error" | "info"
+}
+
+export const PetFeedingTracker = () => {
+    const user = useContext(UserContext);
+    const [flip, setFlip] = useState(0)
+    const [items, setItems] = useState<Pet[]>([])
+    const [petName, setPetName] = useState("")
+    const [petFreq, setPetFreq] = useState(0)
+    const [petIdToEdit, setPetIdToEdit] = useState("")
+    const [editPetName, setEditPetName] = useState("")
+    const [editPetFreq, setEditPetFreq] = useState(0)
+    const [addAnother, setAddAnother] = useState(false)
+    const [toast, setToast] = useState<Toast | null>(null)
+    const [toastTimeout, setToastTimeout] = useState<NodeJS.Timeout>()
+
+    useEffect(() => {
+        loadPets();
+    }, [])
+
+    useEffect(() => {
+        clearTimeout(toastTimeout)
+        if (toast != null) {
+          setToastTimeout(setTimeout(() => {
+            setToast(null)
+          }, 3000));
+        }
+      }, [toast]);
+
+    // Reset after going to main view
+    useEffect(() =>{
+        if (flip === 0) {
+            setPetIdToEdit("")
+            setEditPetName("")
+            setEditPetFreq(0)
+        }
+    }, [flip])
+
+    function newToast(message: string, type: string) {
+        setToast({message, type} as Toast)
+    }
+
+    async function loadPets() {
+        const querySnapshot = await getDocs(
+            query(
+            collection(db, "pets"),
+            where("creatorId", "==", user!.uid)
+            )
+        );
+        const myItems: Pet[] = [];
+        querySnapshot.forEach((doc) => {
+            myItems.push({ ...doc.data(), id: doc.id } as Pet);
+        });
+        setItems(myItems);
+    }
+
+    async function createPet() {
+        if (!petName) {
+            return;
+        }
+        const item = {
+            name: petName,
+            freq_hr: 1,
+            creatorId: user!.uid,
+        }
+        const docRef = await addDoc(collection(db, "pets"), item);
+
+        (item as Pet).id = docRef.id;
+        setItems([...items, item as Pet]);
+    }
+
+    // async function completeGroceryItem(item: Pet) {
+    //     item.isCompleted = true
+    //     deleteDoc(doc(db, "pets", item.id))
+
+    //     const newItems = items.filter(e => e.id != item.id)
+    //     setItems(newItems)
+
+    //     newToast(`Completed ${item.content}`, "success")
+    // }
+
+    function switchToEdit(item: Pet) {
+        setFlip(2)
+        setPetIdToEdit(item.id)
+        setEditPetName(item.name)
+        setEditPetFreq(item.freq_hr)
+    }
+
+    async function editPet(itemContent: string) {
+        const taskDocRef = doc(db, 'pets', petIdToEdit)
+        try{
+            await updateDoc(taskDocRef, {
+                content: itemContent
+            })
+            setItems([])
+            loadPets()
+            setFlip(0)
+            newToast(`Updated "${itemContent}"`, "success")
+        } catch (err) {
+            alert(err)
+            newToast(`Error: Could not update "${itemContent}"`, "error")
+        }   
+    }
+
+    return (
+        <>
+            <div className="widget__header">
+                <div className="widget__title">Pet Feeding Tracker</div>
+                <div className="widget__action">
+                    {
+                        flip === 0 ? 
+                            <button onClick={() => setFlip(1)}><MdAdd className="widget__action__icon"/></button>:
+                        flip === 1 ?
+                            <button onClick={() => setFlip(0)}><MdArrowBack className="widget__action__icon" /></button>:
+                        flip === 2 ?
+                            <button onClick={() => setFlip(0)}><MdArrowBack className="widget__action__icon" /></button>:
+                            <></>
+                    }
+                </div>
+            </div>
+            <div className="widget__content">
+                {
+                    flip === 0 ?
+                        <div>{items.map(item => (
+                            <span key={item.id} className="pet">
+                                <p onClick={() => {switchToEdit(item)}}>{item.name}</p>
+                            </span>
+                        ))}</div>:
+                    flip === 1 ?
+                        <form onSubmit={(e) => {
+                            if (petName != null) {
+                                createPet()
+                            }
+                            if (!addAnother) {
+                                setFlip(0)
+                            }
+                            setPetName("")      // Reset text field after submission
+                            setPetFreq(0)
+                            e.preventDefault()
+                        }}>
+                            <input type="text" placeholder="Grocery item..." value={petName} onChange={(e) => setPetName(e.target.value)} />
+                            <p><input type="checkbox" checked={addAnother} onChange={() => {setAddAnother(!addAnother)}}/>Add another</p>
+                            <input type="submit" value="Save" />
+                        </form>:
+                    flip === 2 ?
+                        <form onSubmit={(e) => {
+                            if (editPetName != null) {
+                                editPet(editPetName)
+                            }
+
+                            e.preventDefault()
+                        }}>
+                            <input type="text" value={editPetName} onChange={(e) => setEditPetName(e.target.value)} />
+                            <input type="submit" value="Save" />
+                        </form>:
+                        <></>
+                }
+            </div>
+            { toast != null ? <div className={`toast toast--${toast.type}`}>{toast.message}</div> : <></> }
+        </>
+    )
+}
