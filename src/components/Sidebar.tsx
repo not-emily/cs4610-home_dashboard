40675aa6a38d8ed1d7e3aa559f2926f99e4cca35
marhurst <a02269647@usu.edu>
2023-04-27 10:26:51 -0600

    logout logic and small auth thing

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,31 @@
 import { useContext } from 'react';
 import UserContext from "../context/user";
 import {MdMenu, MdBreakfastDining, MdLunchDining, MdDinnerDining, MdBakeryDining, MdIcecream, MdLogout, MdPerson, MdArrowBack, MdAccountCircle} from 'react-icons/md'
+import {signOut, getAuth } from 'firebase/auth'
 
 export const Sidebar = (props: {state: boolean, setState: React.Dispatch<React.SetStateAction<boolean>> }) => {
     const user = useContext(UserContext);
+    function logout () {
+        const auth = getAuth()
+        signOut(auth)
+        .then(function() {
+            console.log('weve signed out')
+            // Sign-out successful.
+        })
+        .catch(function(error) {
+            // An error happened
+            console.log('Signout error')
+        });
+    }
+    //<h1 className="sidebar__logo">{user!.email}</h1>
     return (
         <>
             <div className="sidebar__header">
                 <MdAccountCircle className="sidebar__icon"/>
-                <h1 className="sidebar__logo">{user!.email}</h1>
+                <h1 className="sidebar__logo">{user !== null ? user.email : ""}</h1>
             </div>
             <div className="sidebar__items">
-                <div className="item">
+                <div className="item" onClick={() => logout()}>
                     <MdLogout className="sidebar__icon"/>
                     <span className="sidebar__text">Logout</span>
                 </div>
